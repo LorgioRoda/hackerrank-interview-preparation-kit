7ab13b64f136dd413ed967f6793aea2b581606b4
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-07-15 10:21:18 -0400

    Loops solution DONE

---
+++ 10DaysOfJS/day2:Loops.js
@@ -0,0 +1,12 @@
+function vowelsAndConsonants(s) {
+    for(let i = 0; i < s.length ; i++){
+        if(s.charAt(i).match(/[aeiou]/)){
+            console.log(s.charAt(i))
+        }
+    }
+    for(let i = 0; i < s.length ; i++){
+        if(s.charAt(i).match(/[^aeiou]/)){
+            console.log(s.charAt(i))
+        }
+    }
+}
